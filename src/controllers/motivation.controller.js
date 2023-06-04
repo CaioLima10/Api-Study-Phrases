6d27279a5d83e4c8632b92786ef061df63392b29
Caio lima <caiohenrys50@gmail.com>
2023-06-04 14:07:41 -0300

    create controller

--- src/controllers/motivation.controller.js
+++ src/controllers/motivation.controller.js
@@ -2,6 +2,19 @@ import motivationServices from "../services/motivation.services";
 
 
 class MotivationController{
+
+    async create(request, response) {
+        try {
+           const { motivation } = request.body;
+           await motivationServices.create({ motivation });
+     
+           return response.status(201).send();
+     
+         } catch (error) {
+           return response.status(404).json({ message: error.message });
+         }
+       }
+     
     async list(_request, response) {
         try {
           const result = await motivationServices.list();
