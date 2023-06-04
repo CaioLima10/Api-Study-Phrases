394a562b955db2f4410f8328d6b9844202487c03
Caio lima <caiohenrys50@gmail.com>
2023-06-04 13:41:05 -0300

    list motivation

--- src/repositories/phrases.motivation.js
+++ src/repositories/phrases.motivation.js
@@ -1,5 +1,4 @@
 import sqlite3 from 'sqlite3';
-import { randomUUID } from 'crypto';
 import { fileURLToPath } from 'url';
 import path from 'path';
 
@@ -7,3 +6,24 @@ import path from 'path';
 const __filename = fileURLToPath(import.meta.url);
 
 const dbPath = path.resolve(__filename ,'..', 'motivation.db')
+
+class PhrasesMotivation {
+    constructor(){
+      this.db  = new sqlite3.Database(dbPath) 
+    }
+
+
+ async list() {
+    return new Promise((resolve, reject) => {
+      this.db.all('SELECT * FROM phrases', (err, rows) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+      });
+    });
+  }
+}
+
+export default new PhrasesMotivation()
