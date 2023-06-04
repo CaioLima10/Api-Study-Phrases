b39a50a6a19c2295410038db10af0bb08a0ced63
Caio lima <caiohenrys50@gmail.com>
2023-06-04 14:08:20 -0300

    conectando com repository

--- src/services/motivation.services.js
+++ src/services/motivation.services.js
@@ -1,6 +1,20 @@
 import phrasesMotivation from "../repositories/phrases.motivation"
 
 class MotivationService{
+    async create({ motivation }) {
+        try {
+          const phraseMotivationEncontrada = await phrasesMotivation.listByPhraseMotivation({ motivation })
+          
+          if(phraseMotivationEncontrada) {
+            throw new Error('Frase já existe!');
+          }
+        } catch (error) {
+          throw error
+        }
+    
+        return await phrasesMotivation.create({ motivation })
+    }
+
     async list() {
         try {
           return await phrasesMotivation.list()
