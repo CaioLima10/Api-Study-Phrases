f4baa4ed88c84303625b85f78b6a71c0a276416c
Caio lima <caiohenrys50@gmail.com>
2023-06-04 14:07:55 -0300

    create motivation phrases

--- src/repositories/phrases.motivation.js
+++ src/repositories/phrases.motivation.js
@@ -1,4 +1,5 @@
 import sqlite3 from 'sqlite3';
+import { randomUUID } from 'crypto';
 import { fileURLToPath } from 'url';
 import path from 'path';
 
@@ -12,10 +13,39 @@ class PhrasesMotivation {
       this.db  = new sqlite3.Database(dbPath) 
     }
 
+create({  motivation  }){
+    return new Promise((reject , resolve) => {
+
+        const id = randomUUID();
+
+        this.db.run('INSERT INTO phrasesMotivation VALUES(?, ?)' [id , motivation], (err) => {
+            if(err){
+                reject(err)
+            }else{
+                resolve(id)
+            }
+        })
+    })
+}
+
+async listByPhraseMotivation({ motivation }) {
+    return new Promise((resolve, reject) => {
+
+      this.db.get('SELECT * FROM phrases WHERE phrase = ?', motivation, (err, row) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(row);
+        }
+      });
+      
+    });
+  }
+
 
  async list() {
     return new Promise((resolve, reject) => {
-      this.db.all('SELECT * FROM phrases', (err, rows) => {
+      this.db.run('SELECT * FROM phrases', (err, rows) => {
         if (err) {
           reject(err);
         } else {
