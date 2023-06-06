7b45e73d273e97a861e81d6136462c64b96df022
Caio lima <caiohenrys50@gmail.com>
2023-06-06 12:56:36 -0300

    validate priority

--- src/middlewares/phrase.middleware.js
+++ src/middlewares/phrase.middleware.js
@@ -2,12 +2,20 @@
 export function validatePhraseMiddleware(request, response, next) {
   // Executar a lógica do middleware aqui
 
-  const { phrase } = request.body;
+  const { phrase , priority } = request.body;
 
   if(!phrase) {
     return response.status(400).send('"phrase": é obrigatório')
   }
   
+  if(!priority){
+    return response.status(400).send('"priority": é obrigatório')
+  }
+
+  if(priority !== "low" && priority !== "average" && priority !== "high"){
+    return response.status(400).send('"priority": escolha alguma das opção, low , average ou high')
+
+  }
   if(phrase.length < 3) {
     return response.status(400).send('"phrase" precisa ter no minimo 3 caracteres')
   }
