7b5cd6d32fae0456b505639ddd39b8fd5c951637
Caio lima <caiohenrys50@gmail.com>
2023-06-27 00:18:46 -0300

    colocando cors

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { routes } from './routes/index.js';
+import cors from 'cors';
 
 const app = express();
+app.use(cors({ origin: '*' }));
 app.use(bodyParser.json());
 app.use('/', routes);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log(`Rodando na http://localhost:${port}`) 
